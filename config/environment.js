73f949929b27764f4d363a3acbcc8b6a0e7d3ba2
Alise Isaksen <aliseisaksen@utexas.edu>
2017-11-25 21:35:49 -0500

    Modified environment configs

--- config/environment.js
+++ config/environment.js
@@ -44,8 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'none';
-    ENV.rootURL = '/ember-keeper/';
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-keeper';
     //ENV.baseURL = '/';
     // here you can enable a production-specific feature
   }
